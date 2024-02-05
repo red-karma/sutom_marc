864b0fd6ad30202fa8d0bcd2db6d4d4256a26dd5
PositronBeam <eyrignou@yahoo.fr>
2024-02-05 05:14:40 +0100

    Optimization in speed + replaced dict by tuples in several places in order to clean up the code

--- README.md
+++ README.md
@@ -11,7 +11,8 @@ In order to crack it, please call the following functions:
     # Read the dictionary pli07.txt
     words = read_file_dict()
     # Get the words starting with 'S' and containing 7 letters
-    possible_words = get_possible_words(words, 7, known_letters=[{'S': 1}])
+    words = main.keep_words_with_length(words, 7)
+    possible_words = get_possible_words(words, known_letters=[('S', 1)])
     # From this list, get the most probable word
     best_word = get_best_word_from_possibilities(possible_words)
 
@@ -24,8 +25,8 @@ The red letters are in the word to guess, at that place. The orange letters are 
 
     # Read the dictionary pli07.txt
     words = read_file_dict()
-    # Get the words starting with 'S' and containing 7 letters
-    possible_words = get_possible_words(words, 7, known_letters=[{'S': 1}], wrong_place_letters=[{'T': 5}, {'E': 6}, {'R': 7}], forbidden_letters={'A', 'B', 'O'})
+    words = main.keep_words_with_length(words, 7)
+    possible_words = get_possible_words(words, known_letters=[('S': 1)], wrong_place_letters=[('T', 5), ('E', 6), ('R', 7)], forbidden_letters={'A', 'B', 'O'})
     # From this list, get the most probable word
     best_word = get_best_word_from_possibilities(possible_words)
 
@@ -36,7 +37,8 @@ Now, let's imagine that the solver outputs SCRIPTE. Again, you type that word in
 I presume that you see the idea: you have to call the solver with this new result. Just don't forget to put all the forbidden letters:
 
     words = read_file_dict()
-    possible_words = get_possible_words(words, 7, known_letters=[{'S': 1}, {'R': 3}, {'T': 6}, {'E': 7}], wrong_place_letters=[{'I': 4}], forbidden_letters={'A', 'B', 'O', 'C', 'P'})
+    words = main.keep_words_with_length(words, 7)
+    possible_words = get_possible_words(words, known_letters=[('S', 1), ('R', 3), ('T', 6), ('E', 7)], wrong_place_letters=[('I', 4)], forbidden_letters={'A', 'B', 'O', 'C', 'P'})
     best_word = get_best_word_from_possibilities(possible_words)
 
 Now the solver outputs SURDITE, and you win:
@@ -63,4 +65,4 @@ You can also call run_challenge on a list of words to guess, in order to evaluat
 
         nb_attempts = main.run_challenge(['SURDITE', 'SCRUPULE', 'ECOEURE'])
 
-Returns [3, 3, 4]: the solver found 'SURDITE' in 3 attemps, 'SCRUPULE' in 3 attempts, and 'ECOEURE' in 4 attempts.
+Returns [3, 3, 3]: the solver found 'SURDITE' in 3 attemps, 'SCRUPULE' in 3 attempts, and 'ECOEURE' in 3 attempts.
--- test_sutom.py
+++ test_sutom.py
@@ -4,30 +4,35 @@ import main
 
 class MyTestCase(unittest.TestCase):
     def test_get_possible_words_size_10_start_with_Z(self):
-        words = main.read_file_dict()
-        possible_words = main.get_possible_words(words, 10, known_letters=[{'Z': 1}])
+        words = main.read_file()
+        words = main.keep_words_with_length(words, 10)
+        possible_words = main.get_possible_words(words, known_letters=[('Z', 1)])
         self.assertListEqual(possible_words, ['ZEZAIEMENT', 'ZIDOVUDINE', 'ZIGOUILLER', 'ZIGZAGUANT', 'ZIMBABWEEN', 'ZOANTHAIRE', 'ZOOLOGISTE', 'ZOOTECHNIE', 'ZOZOTEMENT', 'ZURICHOISE', 'ZYGOMORPHE', 'ZYGOMYCETE', 'ZYGOPETALE'])  # add assertion here
 
     def test_get_possible_words_size_8_start_with_Z_with_known_letters(self):
-        words = main.read_file_dict()
-        possible_words = main.get_possible_words(words, 8, known_letters=[{'Z': 1}, {'A': 2}])
+        words = main.read_file()
+        words = main.keep_words_with_length(words, 8)
+        possible_words = main.get_possible_words(words, known_letters=[('Z', 1), ('A', 2)])
         self.assertListEqual(possible_words, ['ZAIBATSU', 'ZAIROISE', 'ZAKOUSKI', 'ZANZIBAR', 'ZAPPETTE', 'ZAPPEUSE', 'ZARZUELA'])
 
     def test_get_possible_words_size_8_start_with_Z_with_wrong_letters(self):
-        words = main.read_file_dict()
-        possible_words = main.get_possible_words(words, 8, known_letters=[{'Z': 1}, {'A': 2}], wrong_place_letters=[{'I': 3}])
+        words = main.read_file()
+        words = main.keep_words_with_length(words, 8)
+        possible_words = main.get_possible_words(words, known_letters=[('Z', 1), ('A', 2)], wrong_place_letters=[('I', 3)])
         self.assertListEqual(possible_words, ['ZAKOUSKI', 'ZANZIBAR'])
 
     def test_get_possible_words_size_8_start_with_Z_with_wrong_letters_and_forbidden(self):
-        words = main.read_file_dict()
-        possible_words = main.get_possible_words(words, 8, known_letters=[{'Z': 1}, {'A': 2}], wrong_place_letters=[{'I': 5}], forbidden_letters=['I'])
+        words = main.read_file()
+        words = main.keep_words_with_length(words, 8)
+        possible_words = main.get_possible_words(words, known_letters=[('Z', 1), ('A', 2)], wrong_place_letters=[('I', 5)], forbidden_letters=['I'])
         # Not ZANZIBAR because I would be correct in position 5
         # Not ZAIROISE because the second I is forbidden
         self.assertListEqual(possible_words, ['ZAIBATSU', 'ZAKOUSKI'])
 
     def test_best_word(self):
-        words = main.read_file_dict()
-        possible_words = main.get_possible_words(words, 8, known_letters=[{'A': 1}])
+        words = main.read_file()
+        words = main.keep_words_with_length(words, 8)
+        possible_words = main.get_possible_words(words, known_letters=[('A', 1)])
         best_word = main.get_best_word_from_possibilities(possible_words)
         self.assertEqual(best_word, 'ABORTIVE')
 
@@ -35,9 +40,17 @@ class MyTestCase(unittest.TestCase):
         attempts = main.run_sutom('SURDITE')
         self.assertListEqual(attempts, ['SABOTER', 'SCRIPTE', 'SURDITE'])
 
+    def test_run_sutom_2(self):
+        attempts = main.run_sutom('SCRUPULE')
+        self.assertListEqual(attempts, ['SABOTEUR', 'SCISSURE', 'SCRUPULE'])
+
+    def test_run_sutom_3(self):
+        attempts = main.run_sutom('ECOEURE')
+        self.assertListEqual(attempts, ['ECRIANT', 'ECHOUER', 'ECOEURE'])
+
     def test_run_challenge(self):
         nb_attempts = main.run_challenge(['SURDITE', 'SCRUPULE', 'ECOEURE'])
-        self.assertListEqual(nb_attempts, [3, 3, 4])
+        self.assertListEqual(nb_attempts, [3, 3, 3])
 
 if __name__ == '__main__':
     unittest.main()
